c5c893d1cafff4e06a87bd66d4865d30ef7eaa9f
Taro Fukunaga <tarof429@gmail.com>
2020-01-08 17:05:12 -0800

    Fix broken build script and fix code style

--- src/main/java/pivotsort/PivotSort.java
+++ src/main/java/pivotsort/PivotSort.java
@@ -11,7 +11,7 @@ public class PivotSort {
     // Prints the array
     void printArray(int arr[]) {
         int n = arr.length;
-        for (int i = 0; i < n; ++i)
+        for (int i = 0; i < n; i++)
             System.out.print(arr[i] + " ");
         System.out.println();
     }
